f59da761f0417ebf9c1ca774ee77924ae019c782
Zachary Vacura <zach@hackzzila.com>
2020-05-09 03:25:34 -0500

    start work on safe obs interface

--- src/main.rs
+++ src/main.rs
@@ -1,317 +1,229 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 
-use std::ffi::CStr;
+// #![allow(unused_variables)]
+
 use std::os::raw::{c_char, c_void};
 use winapi::um::winuser::*;
 use winapi::um::libloaderapi::*;
-use std::ptr::{null, null_mut};
-use std::mem::MaybeUninit;
+use std::ptr::{null_mut};
 use resvg::prelude::*;
+use std::error::Error;
 
 use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
 use std::iter::once;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod obs;
+use obs::{Scene, Source, Data, Output};
 
-// unsafe extern fn obs_enum_module_callback(param: *mut c_void, module: *mut obs_module_t) {
-//   println!("enum! {}", *module);
-// }
 
 unsafe extern fn render_window(data: *mut c_void, cx: u32, cy: u32) {
-  // obs_render_main_texture();
-  obs_source_video_render(data as *mut obs_source_t);
+  // obs_source_video_render(data as *mut obs_source_t);
+  obs::obs_render_main_texture();
 }
 
 fn win32_string( value : &str ) -> Vec<u16> {
   OsStr::new( value ).encode_wide().chain( once( 0 ) ).collect()
 }
 
-fn main() {
+fn maifn() -> Result<(), Box<dyn Error>> {
   unsafe {
+    println!("obs version {}", obs::get_version_string()?);
 
-  println!("obs version {}", unsafe { CStr::from_ptr(obs_get_version_string()).to_str().unwrap() });
+    assert!(obs::startup("en-US", None, None)?);
 
-  let startup = obs_startup("en-US\0".as_ptr() as *const c_char, null_mut(), null_mut());
-  // println!("startup: {}", startup);
-  assert!(startup);
-
-  let reset_video =  unsafe {
-    obs_reset_video(Box::into_raw(Box::new(obs_video_info {
+    let reset_video = obs::obs_reset_video(Box::into_raw(Box::new(obs::obs_video_info {
       graphics_module: "libobs-d3d11\0".as_ptr() as *const c_char,
-
       fps_num: 30000,
       fps_den: 1001,
-
       base_width: 1920,
       base_height: 1080,
-
       output_width: 1920,
       output_height: 1080,
-
-
-      // output_format: video_format_VIDEO_FORMAT_UYVY,
-      output_format: video_format_VIDEO_FORMAT_NV12,
-
+      output_format: obs::video_format_VIDEO_FORMAT_NV12,
       adapter: 0,
-
       gpu_conversion: true,
+      colorspace: obs::video_colorspace_VIDEO_CS_DEFAULT,
+      range: obs::video_range_type_VIDEO_RANGE_DEFAULT,
+      scale_type: obs::obs_scale_type_OBS_SCALE_DISABLE,
+    })));
 
-      colorspace: video_colorspace_VIDEO_CS_DEFAULT,
+    assert!(reset_video == obs::OBS_VIDEO_SUCCESS as i32);
 
-      range: video_range_type_VIDEO_RANGE_DEFAULT,
-      scale_type: obs_scale_type_OBS_SCALE_DISABLE,
-    })))
-  };
-
-  assert!(reset_video == OBS_VIDEO_SUCCESS as i32);
-
-  let reset_audio = unsafe {
-    obs_reset_audio(Box::into_raw(Box::new(
-      obs_audio_info {
+    assert!(obs::obs_reset_audio(Box::into_raw(Box::new(
+      obs::obs_audio_info {
         samples_per_sec: 48000,
-        speakers: speaker_layout_SPEAKERS_STEREO,
+        speakers: obs::speaker_layout_SPEAKERS_STEREO,
       }
-    )))
-  };
+    ))));
 
-  assert!(reset_audio);
+    obs::obs_load_all_modules();
+    obs::obs_post_load_modules();
 
-  obs_load_all_modules();
-  obs_post_load_modules();
-  // obs_enum_modules(Some(obs_enum_module_callback), null_mut());
+    let doc = roxmltree::Document::parse(include_str!("../test.svg")).unwrap();
+    let elem = doc.descendants().find(|n| n.attribute("id") == Some("VIDEO")).unwrap();
+    let rect = elem.descendants().find(|n| n.has_tag_name("rect")).unwrap();
 
-  let doc = roxmltree::Document::parse(include_str!("../test.svg")).unwrap();
-  let elem = doc.descendants().find(|n| n.attribute("id") == Some("VIDEO")).unwrap();
-  let rect = elem.descendants().find(|n| n.has_tag_name("rect")).unwrap();
+    let x = rect.attribute("x").unwrap().to_string().parse::<f32>().unwrap();
+    let y = rect.attribute("y").unwrap().to_string().parse::<f32>().unwrap();
+    let width = rect.attribute("width").unwrap().to_string().parse::<f32>().unwrap();
+    let height = rect.attribute("height").unwrap().to_string().parse::<f32>().unwrap();
 
-  let x = rect.attribute("x").unwrap().to_string().parse::<f32>().unwrap();
-  let y = rect.attribute("y").unwrap().to_string().parse::<f32>().unwrap();
-  let width = rect.attribute("width").unwrap().to_string().parse::<f32>().unwrap();
-  let height = rect.attribute("height").unwrap().to_string().parse::<f32>().unwrap();
+    let mut opt = resvg::Options::default();
+    opt.usvg.path = Some("../../../../test.svg".into());
 
-  let mut opt = resvg::Options::default();
-  opt.usvg.path = Some("../../../../test.svg".into());
+    let rtree = usvg::Tree::from_file(&"../../../../test.svg", &opt.usvg).unwrap();
+    let backend = resvg::default_backend();
+    let mut img = backend.render_to_image(&rtree, &opt).unwrap();
+    img.save_png(std::path::Path::new("test.png"));
 
-  let rtree = usvg::Tree::from_file(&"../../../../test.svg", &opt.usvg).unwrap();
-  let backend = resvg::default_backend();
-  let mut img = backend.render_to_image(&rtree, &opt).unwrap();
-  img.save_png(std::path::Path::new("test.png"));
+    // let scene = obs_scene_create("main scene\0".as_ptr() as *const c_char);
+    let scene = Scene::new("main scene")?;
 
-  let scene = obs_scene_create("main scene\0".as_ptr() as *const c_char);
+    let settings = Data::new()?;
+    settings.set_string("file", "test.png")?;
 
-  let settings = obs_data_create();
-  obs_data_set_string(settings, "file\0".as_ptr() as *const c_char, "test.png\0".as_ptr() as *const c_char);
+    // let bg_source = obs_source_create("image_source\0".as_ptr() as *const c_char, "background\0".as_ptr() as *const c_char, settings, null_mut());
+    let bg_source = Source::new("image_source", "background", Some(&settings), None)?;
 
-  let bg_source = obs_source_create("image_source\0".as_ptr() as *const c_char, "background\0".as_ptr() as *const c_char, settings, null_mut());
+    let is_4by3 = true;
 
-  let item = obs_scene_add(scene, bg_source);
+    let item = scene.add(&bg_source)?;
+    item.set_scale(1.0, 1.0);
+    item.set_pos(0.0, 0.0);
 
-  let mut scale = MaybeUninit::uninit();
-  scissors_vec2_set(scale.as_mut_ptr(), 1.0, 1.0);
-  obs_sceneitem_set_scale(item, scale.as_mut_ptr());
+    let vi_source = if false {
+      let vi_source = Source::new("decklink-input", "video", None, None)?;
 
-  let mut pos = MaybeUninit::uninit();
-  scissors_vec2_set(pos.as_mut_ptr(), 0.0, 0.0);
-  obs_sceneitem_set_pos(item, pos.as_mut_ptr());
+      // let props = obs_source_properties(vi_source);
+      let props = vi_source.properties()?;
+      // let prop = obs::obs_properties_get(props, "device_hash\0".as_ptr() as *const c_char);
+      let prop = props.get("device_hash")?;
+      // println!("{}",  prop.list_item_count());
 
-  let settings = obs_data_create();
-  // obs_data_set_string(settings, "device_name\0".as_ptr() as *const c_char, "DeckLink Quad (1)\0".as_ptr() as *const c_char);
-  // obs_data_set_string(settings, "device_hash\0".as_ptr() as *const c_char, "3854888880_DeckLink Quad 2\0".as_ptr() as *const c_char);
-  // obs_data_set_string(settings, "mode_name\0".as_ptr() as *const c_char, "Auto\0".as_ptr() as *const c_char);
-  // obs_data_set_int(settings, "mode_id\0".as_ptr() as *const c_char, -1);
-  // obs_data_set_int(settings, "audio_connection\0".as_ptr() as *const c_char, 1);
-  // obs_data_set_int(settings, "video_connection\0".as_ptr() as *const c_char, 1);
+      for i in 0..prop.list_item_count() {
+        println!("{}", prop.list_item_name(i)?);
+        println!("{}", prop.list_item_string(i)?);
+      }
 
-  // obs_data_set_bool(settings, "looping\0".as_ptr() as *const c_char, true);
+      let dname = prop.list_item_name(1)?;
+      let dstr = prop.list_item_string(1)?;
 
-  // let vi_source = obs_source_create("ffmpeg_source\0".as_ptr() as *const c_char, "video\0".as_ptr() as *const c_char, settings, null_mut());
-  let vi_source = obs_source_create("decklink-input\0".as_ptr() as *const c_char, "video\0".as_ptr() as *const c_char, null_mut(), null_mut());
+      println!("Using: {}", dname);
+      println!("Using: {}", dstr);
 
-  let props = obs_source_properties(vi_source);
-  let prop = obs_properties_get(props, "device_hash\0".as_ptr() as *const c_char);
-  println!("{}", obs_property_list_item_count(prop));
+      // let settings = obs_data_create();
+      // obs_data_set_string(settings, "device_name\0".as_ptr() as *const c_char, dname);
+      // obs_data_set_string(settings, "device_hash\0".as_ptr() as *const c_char, dstr);
+      // obs_data_set_string(settings, "mode_name\0".as_ptr() as *const c_char, "Auto\0".as_ptr() as *const c_char);
+      // obs_data_set_int(settings, "mode_id\0".as_ptr() as *const c_char, -1);
+      // obs_data_set_int(settings, "audio_connection\0".as_ptr() as *const c_char, 1);
+      // obs_data_set_int(settings, "video_connection\0".as_ptr() as *const c_char, 1);
 
-  for i in 0..obs_property_list_item_count(prop) {
-    println!("{}", CStr::from_ptr(obs_property_list_item_name(prop, i)).to_str().unwrap());
-    println!("{}", CStr::from_ptr(obs_property_list_item_string(prop, i)).to_str().unwrap());
-  }
+      let settings = Data::new()?;
+      settings.set_string("device_name", dname)?;
+      settings.set_string("device_hash", dstr)?;
+      settings.set_string("mode_name", "Auto")?;
+      settings.set_int("mode_id", -1)?;
+      settings.set_int("audio_connection", 1)?;
+      settings.set_int("video_connection", 1)?;
+      
+      // obs_source_update(vi_source, settings);
+      vi_source.update(Some(&settings));
 
-  let dname = obs_property_list_item_name(prop, 1);
-  let dstr = obs_property_list_item_string(prop, 1);
+      vi_source
+    } else {
+      let settings = Data::new()?;
+      settings.set_string("file", "../../../../1080img.jpg")?;
 
-  println!("Using: {}", CStr::from_ptr(dname).to_str().unwrap());
-  println!("Using: {}", CStr::from_ptr(dstr).to_str().unwrap());
+      Source::new("image_source", "video", Some(&settings), None)?
+    };
 
-  let settings = obs_data_create();
-  obs_data_set_string(settings, "device_name\0".as_ptr() as *const c_char, dname);
-  obs_data_set_string(settings, "device_hash\0".as_ptr() as *const c_char, dstr);
-  obs_data_set_string(settings, "mode_name\0".as_ptr() as *const c_char, "Auto\0".as_ptr() as *const c_char);
-  obs_data_set_int(settings, "mode_id\0".as_ptr() as *const c_char, -1);
-  obs_data_set_int(settings, "audio_connection\0".as_ptr() as *const c_char, 1);
-  obs_data_set_int(settings, "video_connection\0".as_ptr() as *const c_char, 1);
+    let item = scene.add(&vi_source)?;
+    item.set_scale(width / if is_4by3 { 1440.0 } else { 1920.0 }, height / 1080.0);
+    item.set_pos(x, y);
 
+    if is_4by3 {
+      let settings = Data::new()?;
+      settings.set_int("left", 240)?;
+      settings.set_int("right", 240)?;
 
-  obs_source_update(vi_source, settings);
+      let filter = Source::new("crop_filter", "crop", Some(&settings), None)?;
+      vi_source.filter_add(&filter);
+    }
 
-  // let prop = obs_properties_get(props, "mode_id\0".as_ptr() as *const c_char);
-  // for i in 0..obs_property_list_item_count(prop) {
-  //   println!("{}", CStr::from_ptr(obs_property_list_item_name(prop, i)).to_str().unwrap());
-  //   println!("{}", CStr::from_ptr(obs_property_list_item_string(prop, i)).to_str().unwrap());
-  // }
+    obs::set_output_source(0, &scene.get_source()?);
 
-  // obs_data_set_string(settings, "device_hash\0".as_ptr() as *const c_char, obs_property_list_item_string(prop, 0));
-  // obs_source_update(vi_source, settings);
-
-
-  // panic!();
-
-
-  let item = obs_scene_add(scene, vi_source);
-
-  let mut scale = MaybeUninit::uninit();
-  scissors_vec2_set(scale.as_mut_ptr(), width / 1440.0, height / 1080.0);
-  obs_sceneitem_set_scale(item, scale.as_mut_ptr());
-
-  let mut pos = MaybeUninit::uninit();
-  scissors_vec2_set(pos.as_mut_ptr(), x, y);
-  obs_sceneitem_set_pos(item, pos.as_mut_ptr());
-
-  let settings = obs_data_create();
-  obs_data_set_int(settings, "left\0".as_ptr() as *const c_char, 240);
-  obs_data_set_int(settings, "right\0".as_ptr() as *const c_char, 240);
-  // obs_data_set_int(settings, "width\0".as_ptr() as *const c_char, 1440);
-  // obs_data_set_int(settings, "height\0".as_ptr() as *const c_char, 1080);
-
-  let filter = obs_source_create("crop_filter\0".as_ptr() as *const c_char, "crop\0".as_ptr() as *const c_char, settings, null_mut());
-  obs_source_filter_add(vi_source, filter);
-
-  // obs_data_set_int(settings, "width\0".as_ptr() as *const c_char, width as i64);
-  // obs_data_set_int(settings, "height\0".as_ptr() as *const c_char, height as i64);
-  // obs_data_set_int(settings, "color\0".as_ptr() as *const c_char, 0xFF0000FF);
-  // obs_data_set_string(settings, "color\0".as_ptr() as *const c_char, "#ffffff\0".as_ptr() as *const c_char);
-
-
-  // let source = obs_source_create("color_source\0".as_ptr() as *const c_char, "some random source\0".as_ptr() as *const c_char, null_mut(), null_mut());
-
-
-  // let item = null_mut();
-  // let scale;
-
-  // let mut scale = MaybeUninit::uninit();
-
-  // scissors_vec2_set(scale.as_mut_ptr(), width / 1920.0, height / 1080.0);
-
-  // let item = obs_scene_add(scene, source);
-  // obs_sceneitem_set_scale(item, scale.as_mut_ptr());
-
-  // let mut pos = MaybeUninit::uninit();
-
-  // scissors_vec2_set(pos.as_mut_ptr(), x, y);
-  // obs_sceneitem_set_pos(item, pos.as_mut_ptr());
-
-  obs_set_output_source(0, obs_scene_get_source(scene));
-
-  // obs_source_inc_showing(obs_scene_get_source(scene));
-
-  let handle = unsafe {
     let name = win32_string( "name" );
     let title = win32_string( "title" );
 
     let hinstance = GetModuleHandleW( null_mut() );
     let wnd_class = WNDCLASSW {
-        style : CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
-        lpfnWndProc : Some( DefWindowProcW ),
-        hInstance : hinstance,
-        lpszClassName : name.as_ptr(),
-        cbClsExtra : 0,
-        cbWndExtra : 0,
-        hIcon: null_mut(),
-        hCursor: null_mut(),
-        hbrBackground: null_mut(),
-        lpszMenuName: null_mut(),
+      style : CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
+      lpfnWndProc : Some( DefWindowProcW ),
+      hInstance : hinstance,
+      lpszClassName : name.as_ptr(),
+      cbClsExtra : 0,
+      cbWndExtra : 0,
+      hIcon: null_mut(),
+      hCursor: null_mut(),
+      hbrBackground: null_mut(),
+      lpszMenuName: null_mut(),
     };
 
     RegisterClassW( &wnd_class );
 
-    CreateWindowExW(
-        0,
-        name.as_ptr(),
-        title.as_ptr(),
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        1920,
-        1080,
-        null_mut(),
-        null_mut(),
-        hinstance,
-        null_mut() )
-  };
+    let handle = CreateWindowExW(
+      0,
+      name.as_ptr(),
+      title.as_ptr(),
+      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      1920,
+      1080,
+      null_mut(),
+      null_mut(),
+      hinstance,
+      null_mut()
+    );
 
-  let display = unsafe {
-    obs_display_create(Box::into_raw(Box::new(gs_init_data {
-      window: gs_window { hwnd: handle as *mut std::ffi::c_void },
+    let display = obs::obs_display_create(Box::into_raw(Box::new(obs::gs_init_data {
+      window: obs::gs_window { hwnd: handle as *mut std::ffi::c_void },
       cx: 1920,
       cy: 1080,
-      format: gs_color_format_GS_BGRA,
-      zsformat: gs_zstencil_format_GS_ZS_NONE,
+      format: obs::gs_color_format_GS_BGRA,
+      zsformat: obs::gs_zstencil_format_GS_ZS_NONE,
       adapter: 0,
       num_backbuffers: 0,
-    })), 0xFF0000)
-  };
+    })), 0x000000);
 
-  unsafe {
-    obs_display_add_draw_callback(display, Some(render_window), obs_scene_get_source(scene) as *mut c_void);
+    obs::obs_display_add_draw_callback(display, Some(render_window), null_mut());
 
-    // let vencoder = obs_video_encoder_create("obs_x264\0".as_ptr() as *const c_char, "test_x264\0".as_ptr() as *const c_char, null_mut(), null_mut());
-	  // let aencoder = obs_audio_encoder_create("ffmpeg_aac\0".as_ptr() as *const c_char, "test_aac\0".as_ptr() as *const c_char, null_mut(), 0, null_mut());
+    let output = Output::new("decklink_output", "decklink output", None, None)?;
 
-    // obs_encoder_set_video(vencoder, obs_get_video());
-    // obs_encoder_set_audio(aencoder, obs_get_audio());
-
-    let output = obs_output_create("decklink_output\0".as_ptr() as *const c_char, "decklink output\0".as_ptr() as *const c_char, null_mut(), null_mut());
-
-    let props = obs_source_properties(vi_source);
-    let prop = obs_properties_get(props, "device_hash\0".as_ptr() as *const c_char);
-    let prop_count = obs_property_list_item_count(prop);
-    println!("{}", prop_count);
-
+    let props = vi_source.properties()?;
+    let prop = props.get("device_hash")?;
+    let prop_count = prop.list_item_count();
     for i in 0..prop_count {
-      println!("{}", CStr::from_ptr(obs_property_list_item_name(prop, i)).to_str().unwrap());
-      println!("{}", CStr::from_ptr(obs_property_list_item_string(prop, i)).to_str().unwrap());
+      println!("{}", prop.list_item_name(i)?);
+      println!("{}", prop.list_item_string(i)?);
     }
 
-    let dname = obs_property_list_item_name(prop, prop_count - 1);
-    let dstr = obs_property_list_item_string(prop, prop_count - 1);
+    let dname = prop.list_item_name(prop_count - 1)?;
+    let dstr = prop.list_item_string(prop_count - 1)?;
 
-    println!("Output using: {}", CStr::from_ptr(dname).to_str().unwrap());
-    println!("Output using: {}", CStr::from_ptr(dstr).to_str().unwrap());
+    println!("Output using: {}", dname);
+    println!("Output using: {}", dstr);
 
-    let settings = obs_data_create();
-    obs_data_set_string(settings, "device_name\0".as_ptr() as *const c_char, dname);
-    obs_data_set_string(settings, "device_hash\0".as_ptr() as *const c_char, dstr);
-    obs_data_set_string(settings, "mode_name\0".as_ptr() as *const c_char, "1080i59.94\0".as_ptr() as *const c_char);
-    obs_data_set_int(settings, "mode_id\0".as_ptr() as *const c_char, 12);
-    // obs_data_set_int(settings, "audio_connection\0".as_ptr() as *const c_char, 1);
-    // obs_data_set_int(settings, "video_connection\0".as_ptr() as *const c_char, 1);
+    let settings = Data::new()?;
+    settings.set_string("device_name", dname);
+    settings.set_string("device_hash", dstr);
+    settings.set_string("mode_name", "1080i59.94");
+    settings.set_int("mode_id", 12);
 
-    // "mode_id": 12,
-    // "mode_name": "1080i59.94",
+    output.update(Some(&settings));
 
-    // let settings = obs_data_create();
-    // obs_data_set_string(settings, "path\0".as_ptr() as *const c_char, "test.flv\0".as_ptr() as *const c_char);
-
-    // obs_output_set_video_encoder(output, vencoder);
-    // obs_output_set_audio_encoder(output, aencoder, 0);
-
-    obs_output_update(output, settings);
-
-    println!("{}", obs_output_start(output));
-
-
+    assert!(output.start());
     loop {
       let mut message : MSG = std::mem::uninitialized();
       if GetMessageW( &mut message as *mut MSG, handle, 0, 0 ) > 0 {
@@ -321,7 +233,11 @@ fn main() {
           break;
       }
     }
-  }
 
+    Ok(())
   }
+}
+
+fn main() {
+  maifn().unwrap();
 }
---
+++ src/obs.rs
@@ -0,0 +1,382 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::fmt;
+use std::error::Error;
+use std::ptr::{null, null_mut};
+use std::mem::MaybeUninit;
+use std::ffi::{CStr, CString};
+
+#[derive(Debug, Clone)]
+pub struct NullError;
+
+impl fmt::Display for NullError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "obs api returned nullptr")
+  }
+}
+
+impl Error for NullError {
+  fn source(&self) -> Option<&(dyn Error + 'static)> {
+    None
+  }
+}
+
+pub struct Scene {
+  ptr: *mut obs_scene_t,
+}
+
+impl Scene {
+  pub fn new(name: &str) -> Result<Self, Box<dyn Error>> {
+    let name = CString::new(name)?;
+    let ptr = unsafe {
+      obs_scene_create(name.as_ptr())
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      unsafe { obs_scene_addref(ptr) }
+      Ok(Self { ptr })
+    }
+  }
+
+  pub fn add(&self, source: &Source) -> Result<SceneItem, Box<dyn Error>> {
+    let ptr = unsafe {
+      obs_scene_add(self.ptr, source.ptr)
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      Ok(SceneItem::from(ptr))
+    }
+  }
+
+  pub fn get_source(&self) -> Result<Source, Box<dyn Error>> {
+    let ptr = unsafe {
+      obs_scene_get_source(self.ptr)
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      Ok(Source::from(ptr))
+    }
+  }
+}
+
+impl Drop for Scene {
+  fn drop(&mut self) {
+    unsafe {
+      obs_scene_release(self.ptr);
+    }
+  }
+}
+
+pub struct SceneItem {
+  ptr: *mut obs_sceneitem_t,
+}
+
+impl SceneItem {
+  pub fn set_scale(&self, x: f32, y: f32) {
+    let mut vec = MaybeUninit::uninit();
+    unsafe {
+      scissors_vec2_set(vec.as_mut_ptr(), x, y);
+      obs_sceneitem_set_scale(self.ptr, vec.as_mut_ptr());
+    }
+  }
+
+  pub fn set_pos(&self, x: f32, y: f32) {
+    let mut vec = MaybeUninit::uninit();
+    unsafe {
+      scissors_vec2_set(vec.as_mut_ptr(), x, y);
+      obs_sceneitem_set_pos(self.ptr, vec.as_mut_ptr());
+    }
+  }
+}
+
+impl From<*mut obs_sceneitem_t> for SceneItem {
+  fn from(ptr: *mut obs_sceneitem_t) -> Self {
+    unsafe { obs_sceneitem_addref(ptr) }
+    Self { ptr }
+  }
+}
+
+impl Drop for SceneItem {
+  fn drop(&mut self) {
+    unsafe {
+      obs_sceneitem_release(self.ptr);
+    }
+  }
+}
+
+pub struct Source {
+  ptr: *mut obs_source_t,
+}
+
+impl Source {
+  pub fn new(id: &str, name: &str, settings: Option<&Data>, hotkey_data: Option<&Data>) -> Result<Self, Box<dyn Error>> {
+    let id = CString::new(id)?;
+    let name = CString::new(name)?;
+    let ptr = unsafe {
+      obs_source_create(
+        id.as_ptr(),
+        name.as_ptr(),
+        settings.map_or(null_mut(), |x| x.ptr),
+        hotkey_data.map_or(null_mut(), |x| x.ptr)
+      )
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      unsafe { obs_source_addref(ptr) }
+      Ok(Self { ptr })
+    }
+  }
+
+  pub fn update(&self, settings: Option<&Data>) {
+    unsafe {
+      obs_source_update(self.ptr, settings.map_or(null_mut(), |x| x.ptr));
+    }
+  }
+
+  pub fn properties(&self) -> Result<Properties, Box<dyn Error>> {
+    let ptr = unsafe {
+      obs_source_properties(self.ptr)
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      Ok(Properties::from(ptr))
+    }
+  }
+
+  pub fn filter_add(&self, filter: &Source) {
+    unsafe {
+      obs_source_filter_add(self.ptr, filter.ptr);
+    }
+  }
+}
+
+impl From<*mut obs_source_t> for Source {
+  fn from(ptr: *mut obs_source_t) -> Self {
+    unsafe { obs_source_addref(ptr) }
+    Self { ptr }
+  }
+}
+
+
+impl Drop for Source {
+  fn drop(&mut self) {
+    unsafe {
+      obs_source_release(self.ptr);
+    }
+  }
+}
+
+pub struct Data {
+  ptr: *mut obs_data_t,
+}
+
+impl Data {
+  pub fn new() -> Result<Self, Box<dyn Error>> {
+    let ptr = unsafe {
+      obs_data_create()
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      unsafe { obs_data_addref(ptr) }
+      Ok(Self { ptr })
+    }
+  }
+
+  pub fn set_string(&self, key: &str, value: &str) -> Result<(), Box<dyn Error>> {
+    let key = CString::new(key)?;
+    let value = CString::new(value)?;
+    unsafe {
+      obs_data_set_string(self.ptr, key.as_ptr(), value.as_ptr());
+    }
+    Ok(())
+  }
+
+  pub fn set_int(&self, key: &str, value: i64) -> Result<(), Box<dyn Error>> {
+    let key = CString::new(key)?;
+    unsafe {
+      obs_data_set_int(self.ptr, key.as_ptr(), value);
+    }
+    Ok(())
+  }
+}
+
+impl Drop for Data {
+  fn drop(&mut self) {
+    unsafe {
+      obs_data_release(self.ptr);
+    }
+  }
+}
+
+pub struct Properties {
+  ptr: *mut obs_properties_t,
+}
+
+impl Properties {
+  pub fn get(&self, key: &str) -> Result<Property, Box<dyn Error>> {
+    let key = CString::new(key)?;
+    let ptr = unsafe {
+      obs_properties_get(self.ptr, key.as_ptr())
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      Ok(Property::from(ptr))
+    }
+  }
+}
+
+impl From<*mut obs_properties_t> for Properties {
+  fn from(ptr: *mut obs_properties_t) -> Self {
+    Self { ptr }
+  }
+}
+
+impl Drop for Properties {
+  fn drop(&mut self) {
+    unsafe {
+      obs_properties_destroy(self.ptr);
+    }
+  }
+}
+
+pub struct Property {
+  ptr: *mut obs_property_t,
+}
+
+impl Property {
+  pub fn list_item_count(&self) -> u64 {
+    unsafe {
+      obs_property_list_item_count(self.ptr)
+    }
+  }
+
+  pub fn list_item_name(&self, index: u64) -> Result<&str, Box<dyn Error>> {
+    let ptr = unsafe {
+      obs_property_list_item_name(self.ptr, index)
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      Ok(unsafe { CStr::from_ptr(ptr).to_str()? })
+    }
+  }
+
+  pub fn list_item_string(&self, index: u64) -> Result<&str, Box<dyn Error>> {
+    let ptr = unsafe {
+      obs_property_list_item_string(self.ptr, index)
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      Ok(unsafe { CStr::from_ptr(ptr).to_str()? })
+    }
+  }
+}
+
+impl From<*mut obs_property_t> for Property {
+  fn from(ptr: *mut obs_property_t) -> Self {
+    Self { ptr }
+  }
+}
+
+pub struct Output {
+  ptr: *mut obs_output_t,
+}
+
+impl Output {
+  pub fn new(id: &str, name: &str, settings: Option<&Data>, hotkey_data: Option<&Data>) -> Result<Self, Box<dyn Error>> {
+    let id = CString::new(id)?;
+    let name = CString::new(name)?;
+    let ptr = unsafe {
+      obs_output_create(
+        id.as_ptr(),
+        name.as_ptr(),
+        settings.map_or(null_mut(), |x| x.ptr),
+        hotkey_data.map_or(null_mut(), |x| x.ptr)
+      )
+    };
+
+    if ptr == null_mut() {
+      Err(Box::new(NullError))
+    } else {
+      unsafe { obs_output_addref(ptr) }
+      Ok(Self { ptr })
+    }
+  }
+
+  pub fn update(&self, settings: Option<&Data>) {
+    unsafe {
+      obs_output_update(self.ptr, settings.map_or(null_mut(), |x| x.ptr));
+    }
+  }
+  
+  pub fn start(&self) -> bool {
+    unsafe {
+      obs_output_start(self.ptr)
+    }
+  }
+}
+
+impl From<*mut obs_output_t> for Output {
+  fn from(ptr: *mut obs_output_t) -> Self {
+    unsafe { obs_output_addref(ptr) }
+    Self { ptr }
+  }
+}
+
+
+impl Drop for Output {
+  fn drop(&mut self) {
+    unsafe {
+      obs_output_release(self.ptr);
+    }
+  }
+}
+
+pub fn get_version_string() -> Result<&'static str, Box<dyn Error>> {
+  unsafe {
+    Ok(CStr::from_ptr(obs_get_version_string()).to_str()?)
+  }
+}
+
+pub fn startup(locale: &str, module_config_path: Option<&str>, store: Option<*mut profiler_name_store_t>) -> Result<bool, Box<dyn Error>> {
+  let locale = CString::new(locale)?;
+  let mut _module_config_path_string = CString::new("")?;
+  let mut module_config_path_ptr = null();
+  if let Some(string) = module_config_path {
+    _module_config_path_string = CString::new(string)?;
+    module_config_path_ptr = _module_config_path_string.as_ptr();
+  }
+  let store = store.unwrap_or(null_mut());
+  unsafe {
+    Ok(obs_startup(locale.as_ptr(), module_config_path_ptr, store))
+  }
+}
+
+pub fn set_output_source(index: u32, source: &Source) {
+  unsafe {
+    obs_set_output_source(index, source.ptr);
+  }
+}
